b9f26085fc0db183a7ad89fb6ff5332a824f53f4
DengsongWang <90630362+DengsongWang@users.noreply.github.com>
2023-09-29 03:37:39 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
+import face from "./img/hezhao.jpg";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={face} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ok <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React hh
         </a>
       </header>
     </div>
